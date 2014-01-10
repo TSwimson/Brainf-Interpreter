7feb78fa0c0c933555730a4b15dc112cb9ae6057
TSwimson <TannerSwenson@gmail.com>
2014-01-10 10:54:53 -0800

    Refactored interpreter into a class

--- interpreter.rb
+++ interpreter.rb
@@ -1,82 +1,131 @@
-user_wants_to_exit = false
-#Main loop to allow the loading of multiple programs
-until user_wants_to_exit
+class Interpreter
+  def initialize
+    @instructions = []
+    @data = [0]*30000
+    @instruction_pointer = 0
+    @data_pointer = 0
+    @output = ""
+    start
+  end
 
-  have_file = false
-  instructions = []
-  puts "[I]nteractive mode or load [P]rogram?"
-  #loops to get file succesfully
-  until have_file
+  def start
+    prompt_mode
+  end
 
+  def prompt_mode
+    while true
+      #puts "[I]nteractive mode, Load [P]rogram, or [E]xit?"
+      puts "Load [P]rogram or [E]xit?"
+      case gets.chomp.downcase
+      when "i"
+      when "p"
+        load_program
+        prompt_to_run_program
+      when "e"
+        break
+      end
+    end
+  end
+
+  def prompt_to_run_program
+    while true
+      puts "Do you want to [R]un or [O]utput the program or go [B]ack?"
+      case gets.chomp.downcase
+      when "r"
+        run_program
+      when "o"
+        print_program
+      when "b"
+        break
+      end
+    end
+  end
+
+  def print_program
+    @instructions.each { |i| print i }
+    puts
+  end
+
+  def run_program
+    while @instruction_pointer < @instructions.length
+      act_on_instruction(@instructions[@instruction_pointer])
+      update_display
+      @instruction_pointer += 1
+    end
+  end
+
+  def update_display
+    system "clear"
+    @data[0..10].each { |x| print x.to_s + " "}
+    puts "\n" << @output
+  end
+
+  def get_file_name
     puts "Enter the name of file to open "
-    file_name = gets.chomp
-
-    begin
-      File.open(file_name) do |file|
-        while i = file.getc
-          instructions.push i
-        end
-      end
-      puts "File Loaded: " + instructions.to_s
-      have_file = true
-
-    rescue Errno::ENOENT => error
-      puts "File not found: " + error.to_s
-      have_file = false
-    end
-  end
-  #file has been retrieve now beginning to run the program
-  #wrapper loop to allow restarting program
-  run_program = true
-  while run_program
-    instruction_pointer = 0
-    data = [0]*30000
-    data_pointer = 0
-    output = ""
-    #big case statement for each instruction
-    while instruction_pointer < instructions.length
-      case instructions[instruction_pointer]
-      when ">"
-        data_pointer += 1 if data_pointer + 1 < 30000
-      when "<"
-        data_pointer -= 1 if data_pointer > 0
-      when "+"
-        data[data_pointer] += 1
-        data[data_pointer] = 0 if data[data_pointer] > 255
-      when "-"
-        data[data_pointer] -= 1
-        data[data_pointer] = 255 if data[data_pointer] < 0
-      when "."
-        o = data[data_pointer].chr
-        output += o if o
-      when ","
-        puts "Enter a number 0-255"
-        i = gets.to_i
-        data[data_pointer] = i if i <= 255 && i >= 0
-      when "["
-        if data[data_pointer] == 0
-          while instructions[instruction_pointer] != "]"
-            instruction_pointer += 1
-          end
-        end
-      when "]"
-        if data[data_pointer] != 0
-          while instructions[instruction_pointer] != "["
-            instruction_pointer -= 1
-          end
-        end
-      end
-      #to show whats going on update the display with data array after each instruction
-      system "clear"
-      data[0..10].each { |x| print x.to_s + " "}
-      puts
-      instruction_pointer += 1
-    end
-    puts output
-    puts "Do you want to restart the program [y/n]"
-    run_program = gets.chomp == "y"
+    gets.chomp
   end
 
-  puts "Exit? [y/n]"
-  user_wants_to_exit = gets.chomp == "y"
+  def load_program
+
+    have_file = false
+    @instructions = []
+    until have_file
+
+      file_name = get_file_name
+
+      begin
+        File.open(file_name) do |file|
+          while i = file.getc
+            @instructions.push i
+          end
+        end
+
+        have_file = true
+
+      rescue Errno::ENOENT => error
+        puts "File not found: " + error.to_s
+        have_file = false
+      end
+    end
+  end
+
+  def act_on_instruction(ins)
+    case ins
+    when ">"
+      @data_pointer += 1 if @data_pointer + 1 < 30000
+    when "<"
+      @data_pointer -= 1 if @data_pointer > 0
+    when "+"
+      @data[@data_pointer] += 1
+      @data[@data_pointer] = 0 if @data[@data_pointer] > 255
+    when "-"
+      @data[@data_pointer] -= 1
+      @data[@data_pointer] = 255 if @data[@data_pointer] < 0
+    when "."
+      o = @data[@data_pointer].chr
+      @output += o if o
+    when ","
+      get_input
+    when "["
+      if @data[@data_pointer] == 0
+        while @instructions[@instruction_pointer] != "]"
+          @instruction_pointer += 1
+        end
+      end
+    when "]"
+      if @data[@data_pointer] != 0
+        while @instructions[@instruction_pointer] != "["
+          @instruction_pointer -= 1
+        end
+      end
+    end
+  end
+
+  def get_input
+    puts "Enter a number 0-255"
+    i = gets.to_i
+    @data[@data_pointer] = i if i <= 255 && i >= 0
+  end
 end
+
+i = Interpreter.new
